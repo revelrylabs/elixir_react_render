1dd1af2fcabb26fabf321dfc72f73deceb95aef4
Bryan Joseph <bryanjos@gmail.com>
2019-02-22 08:22:44 -0600

    Update version for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReactRender.MixProject do
   def project do
     [
       app: :react_render,
-      version: "2.0.1",
+      version: "2.0.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
