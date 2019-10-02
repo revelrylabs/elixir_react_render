417f7674646cfe95a140330e72b906f45f460c6d
Hung Tran <hung@0x0.ninja>
2019-10-02 11:20:20 -0500

    bump nodejs dependency

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule ReactRender.MixProject do
       {:jason, "~> 1.0"},
       {:ex_doc, "~> 0.21.2", only: :dev},
       {:excoveralls, "~> 0.11.2", only: :test},
-      {:nodejs, "~> 1.0"}
+      {:nodejs, "~> 1.1"}
     ]
   end
 
