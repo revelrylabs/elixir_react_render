e78b48c82791778e97be253f4f8dbf0ff6b31ad3
Hung Tran <hung@0x0.ninja>
2020-08-22 11:22:35 -0500

    add test case for console.log

--- test/react_render_test.exs
+++ test/react_render_test.exs
@@ -30,6 +30,14 @@ defmodule ReactRender.Test do
       assert html =~ "test</div>"
     end
 
+    test "does not crash with console.log" do
+      {:safe, html} = ReactRender.render("ConsoleLog.js", %{})
+      assert html =~ "data-rendered"
+      assert html =~ "data-component"
+      assert html =~ "TestComponent"
+      assert html =~ "<div data-reactroot=\"\">console.*"
+    end
+
     test "raises RenderError when no component found" do
       assert_raise ReactRender.RenderError, ~r/Cannot find module/, fn ->
         ReactRender.render("./NotFound.js")
