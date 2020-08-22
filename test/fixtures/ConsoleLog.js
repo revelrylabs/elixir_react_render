1a3ce47a7a8f802a2a88eabccedd29560671b34a
Hung Tran <hung@0x0.ninja>
2020-08-22 11:22:35 -0500

    add test case for console.log

---
+++ test/fixtures/ConsoleLog.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+function TestComponent(props) {
+  console.log('should not crash')
+
+  return (
+    <div>console.*</div>
+  )
+}
+
+export default TestComponent
